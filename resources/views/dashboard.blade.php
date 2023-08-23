53eb67978fa7f0d022695115464562d4659a8755
IbnuSirin02 <sirin21dec@gmail.com>
2023-08-23 16:16:59 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+  
+@section('title', 'Dashboard - Laravel Admin Panel With Login Logout and CRUD')
+  
+@section('contents')
+  <div class="row">
+  
+  </div>
+@endsection
